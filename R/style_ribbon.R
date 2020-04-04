7656108bf05c182e1503035bd07bbf2ed8484012
Shornone <36076818+Shornone@users.noreply.github.com>
2020-04-04 09:19:21 -0700

    Update style_ribbon.R

--- R/style_ribbon.R
+++ R/style_ribbon.R
@@ -98,8 +98,10 @@ style_ribbon <- function(
 
     ggplot2::coord_equal(
 #    ggplot2::coord_polar(
-      xlim = c(-.05, 1.05),
-      ylim = c(-.05, 1.05)
+#      xlim = c(-.05, 1.05),
+#      ylim = c(-.05, 1.05)
+      xlim = c(-5, 5),
+      ylim = c(-5, 5)
     )
 
   # add the plot information
